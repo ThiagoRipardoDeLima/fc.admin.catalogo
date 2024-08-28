ca6792ed45a8f37934aa52db98c1d2b7614ef563
Thiago Ripardo de Lima <thiago.ripardo.86@gmail.com>
2024-08-28 10:17:02 -0400

    Finalizando projeto base

--- infrastructure/src/main/java/com/fc/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fc/admin/catalogo/infrastructure/Main.java
@@ -1,7 +1,10 @@
 package com.fc.admin.catalogo.infrastructure;
 
+import com.fc.admin.catalogo.application.UseCase;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(new UseCase().execute());
     }
 }
